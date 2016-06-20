8aa97b0a3b14cdc524ef46a3dbfe46ff3720646b
Joel Robert Justiawan <joelwindows7@gmail.com>
2016-06-20 17:41:52 +0700

    NEW! ROLL!
    Serveral changes, esp. those Vertical roll textures and Lua updates I can't guarantee!
    Please help me to choose another engine!!! I want to make this universal (all gametype)!

--- NewSkins/vacuum-ship/noteskin.lua
+++ NewSkins/vacuum-ship/noteskin.lua
@@ -5,14 +5,17 @@
 --Nor known to life
 --Have withstood pain to create many noteskins
 --Yet these hands will never hold anything
---So as I pray, Unlimited Stepman Works
+--So as I pray, Unlimited Stepman Works -Jousway-
 
 return {
 	notes= "notes.lua",
 	layers= {"receptors.lua", "explosions.lua"},
-	supports_all_buttons= false,
+	supports_all_buttons= true,
 	buttons= {"Left", "Down", "Up", "Right", "UpLeft", "UpRight"},
 	fallback= "",
-	skin_parameters= {},
-	skin_parameter_info= {},
+	skin_parameters= {
+		receptors = {
+			warning_time = 1,},
+	skin_parameter_info= {
+		},
 }
